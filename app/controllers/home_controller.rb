b018cec9d43436b84e87145c657b68ba9bcbf22c
Masaya Nasu <tomato.wonder.life@gmail.com>
2016-03-01 01:11:41 +0900

    FIX #8 ページネーションの追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,11 @@
 class HomeController < ApplicationController
   def index
-    @timelines = Timeline.includes(user_book: [:user, :book]).all.order(created_at: :DESC)
+    @timelines = Timeline.includes(user_book: [:user, :book]).page(page[:page]).order(created_at: :DESC)
+  end
+
+  private
+
+  def page
+    params.permit(:page)
   end
 end
